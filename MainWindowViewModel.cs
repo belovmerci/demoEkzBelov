1c2108e50cbe38fa5ceadc2bb8fa14b109b2ce73
belovaa <belovaa@mer.ci.nsu.ru>
2024-05-06 12:19:32 +0700

    mostly done

--- MainWindowViewModel.cs
+++ MainWindowViewModel.cs
@@ -27,13 +27,8 @@ namespace demoEkzBelov
                 PerformSearchAndSort();
 }
         }
-        
 
         private ObservableCollection<ProductModel> _products = new ObservableCollection<ProductModel>();
-
-        private int _currentPageIndex = 0;
-        private int _pageSize = 10;
-
         public ObservableCollection<ProductModel> Products
         {
             get { return _products; }
@@ -41,6 +36,20 @@ namespace demoEkzBelov
             {
                 _products = value;
                 OnPropertyChanged(nameof(Products));
+            }
+        }
+
+
+        private int _currentPageIndex = 0;
+        private int _pageSize = 10;
+        private ObservableCollection<ProductModel> _currentProducts = new ObservableCollection<ProductModel>();
+        public ObservableCollection<ProductModel> CurrentProducts
+        {
+            get { return _currentProducts; }
+            set
+            {
+                _currentProducts = value;
+                OnPropertyChanged(nameof(CurrentProducts));
             }
         }
 
@@ -54,6 +63,8 @@ namespace demoEkzBelov
                 PerformSearchAndSort();
             }
         }
+
+        public ObservableCollection<string> Manufacturers { get; set; }
 
         private string _selectedManufacturer;
         public string SelectedManufacturer
@@ -107,7 +118,7 @@ namespace demoEkzBelov
             {
                 _currentPage = value;
                 OnPropertyChanged(nameof(CurrentPage));
-                UpdateProducts();
+                LoadCurrentPageItems();
             }
         }
 
@@ -122,7 +133,7 @@ namespace demoEkzBelov
             var manufacturers = OriginalProducts.Select(p => p.Manufacturer).Distinct().ToList();
             manufacturers.Insert(0, "All Manufacturers");
             Manufacturers = new ObservableCollection<string>(manufacturers);
-            SelectedManufacturer = "All Manufacturers";
+            _selectedManufacturer = "All Manufacturers";
 
             PerformSearchAndSort();
         }
@@ -144,12 +155,13 @@ namespace demoEkzBelov
         {
             // Filter products based on search text and manufacturer
             var filteredProducts = OriginalProducts.Where(p =>
-                (string.IsNullOrWhiteSpace(SearchText) || p.Name.ToLower().Contains(SearchText.ToLower()) ||
+                (string.IsNullOrWhiteSpace(SearchText) ||
+                p.Name.ToLower().Contains(SearchText.ToLower()) ||
                 p.Description.ToLower().Contains(SearchText.ToLower())) &&
                 (SelectedManufacturer == "All Manufacturers" || p.Manufacturer == SelectedManufacturer))
                 .ToList();
 
-            // Sort filtered products based on price
+            // Sort filtered ascending/descending by price
             if (SortAscending)
             {
                 filteredProducts = filteredProducts.OrderBy(p => p.Price).ToList();
@@ -163,29 +175,46 @@ namespace demoEkzBelov
             TotalEntries = filteredProducts.Count;
             TotalPages = (int)Math.Ceiling((double)TotalEntries / _pageSize);
 
-            // Update the Products collection on the UI thread
-            Application.Current.Dispatcher.Invoke(() =>
+            Products.Clear();
+            foreach (var product in filteredProducts)
             {
-                _products.Clear();
-                foreach (var product in filteredProducts.Skip(_currentPageIndex * _pageSize).Take(_pageSize))
-                {
-                    _products.Add(product);
-                }
-            });
+                Products.Add(product);
+            }
+
+            // Load 1st page of updated items
+            CurrentPage = 1;
+            // LoadCurrentPageItems();
+            // Is called on updating CurrentPage
         }
 
-        private void UpdateProducts()
+        private void LoadCurrentPageItems()
         {
-            PerformSearchAndSort();
+            // Anticipating user induced errors
+            if (_currentPage > TotalPages)
+            {
+                _currentPage = _totalPages;
+            }
+            else if (_currentPage < 1)
+            {
+                _currentPage = 1;
+            }
+
+            // Based of current page make a selection to show
+            CurrentProducts.Clear();
+            foreach (var product in Products.Skip(_currentPageIndex * _pageSize).Take(_pageSize))
+            {
+                CurrentProducts.Add(product);
+            }
         }
 
         protected virtual void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
-
-        public ObservableCollection<string> Manufacturers { get; set; }
     }
+
+
+
 
     // Converter to convert availability boolean to color
     public class AvailabilityToColorConverter : IValueConverter
